dcd52571273deeaea0d3c1362c26261def82448f
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2014-08-23 16:10:50 +0300

    build TheProject in runtime

--- ProjectBuilder/Program.cs
+++ ProjectBuilder/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Execution;
 
 namespace ProjectBuilder
 {
@@ -9,6 +11,18 @@ namespace ProjectBuilder
     {
         static void Main(string[] args)
         {
+            try
+            {
+                var result = BuildManager.DefaultBuildManager.Build(
+                    new BuildParameters(),
+                    new BuildRequestData(
+                        new ProjectInstance(@"..\..\..\TheProject\TheProject.csproj"), new[] {"Rebuild"}));
+                Console.WriteLine("OveralResult: {0}", result.OverallResult);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed with exception: {0}", e);
+            }
         }
     }
 }
